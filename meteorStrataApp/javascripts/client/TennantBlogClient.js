edd05d0a6aa29bb8cd5860b11d33bbfd08f5f990
Ciaran Hale <ciaranhale@gmail.com>
2015-03-25 20:37:41 +1100

    [strataApp-24] Move Tennant Blog into new structure

---
+++ meteorStrataApp/javascripts/client/TennantBlogClient.js
@@ -0,0 +1,60 @@
+/*global Meteor, Template, BlogPosts, BlogPostService*/
+
+/**
+ * Separate player logic into an own service singleton for better testability and reusability.
+ * @type {{}}
+ */
+BlogPostService = {
+	blogPosts: function() {
+		return BlogPosts.find({});
+	},
+	postCount: function() {
+		return BlogPosts.find().count();
+	}
+};
+
+
+// This code only runs on the client
+Meteor.subscribe('blogPosts');
+
+Template.tenantBlog.helpers({
+	blogPosts: function() {
+		return BlogPostService.blogPosts();
+	},
+	postCount: function() {
+		return BlogPostService.postCount();
+	}
+
+});
+
+Template.tenantBlog.events({
+	'submit .newBlogPostForm': function(event) {
+		// This function is called when the new task form is submitted
+
+		var text = event.target.text.value;
+
+		Meteor.call('addPost', text);
+
+		// Clear form
+		event.target.text.value = '';
+
+		// Prevent default form submit
+		return false;
+	}
+});
+
+Template.post.events({
+	'click .delete': function() {
+		Meteor.call('deletePost', this._id);
+	}
+});
+
+
+Template.post.helpers({
+	isOwner: function() {
+		return this.owner === Meteor.userId();
+	}
+});
+
+
+
