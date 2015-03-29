a13f1773f45e791ef305e7ebc1695af114273eff
Ciaran Hale <ciaranhale@gmail.com>
2015-03-29 15:34:55 +1100

    [strataApp-29] Add User Tests for chat

--- meteorStrataApp/tests/cucumber/features/blockChat.feature
+++ meteorStrataApp/tests/cucumber/features/blockChat.feature
@@ -6,9 +6,11 @@ Feature: Strata App Block Chat Page
 Background: Initial State
 	Given I am on the Chat Page
 	When I navigate to "blockChat"
-	Then Logout if already signed in
-	Then I login from a mobile
+
 
 Scenario: Initial State of Chat Page
 	Then I should see a input for new chat messages with an id of ".sendChatMessage"
 
+Scenario: Sending a Message
+	Then I select a user to text
+	Then I enter a message of "Hello How are you"
--- meteorStrataApp/tests/cucumber/features/step_definitions/blockChatSteps.js
+++ meteorStrataApp/tests/cucumber/features/step_definitions/blockChatSteps.js
@@ -22,16 +22,31 @@
 
 		this.Then(/^I should see a input for new chat messages with an id of "([^"]*)"$/, function(expectedForm, next) {
 			helper.world.browser
-			.waitFor('#message',200)
-			.isExisting(expectedForm, function(err, isExisting) {
-				if (!isExisting) {
-					next.fail('The Element ' + expectedForm + ' is not existing on the page');
-				} else {
-					next();
-				}
+				.waitFor('#message', 200)
+				.isExisting(expectedForm, function(err, isExisting) {
+					if (!isExisting) {
+						next.fail('The Element ' + expectedForm + ' is not existing on the page');
+					} else {
+						next();
+					}
 
-			});
+				});
 
+		});
+
+		this.Then(/^I select a user to text$/, function(callback) {
+			helper.world.browser
+				.waitFor('.loggedOutUser', 200)
+				.click('.loggedOutUser')
+				.call(callback);
+		});
+
+		this.Then(/^I enter a message of "([^"]*)"$/, function(messageText, callback) {
+			helper.world.browser
+				.waitFor('.form-control', 200)
+				.setValue('.form-control', messageText)
+				.click('.sendChatMessage')
+				.call(callback);
 		});
 
 	};
